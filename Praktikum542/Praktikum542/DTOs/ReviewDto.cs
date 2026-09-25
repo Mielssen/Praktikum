7178fb28d1ca9dd0730d5acaf6c5612cc96dd021
Twiniii <kasapchuktanya@gmail.com>
2026-09-25 23:27:51 +0300

    Add files via upload

---
+++ Praktikum542/Praktikum542/DTOs/ReviewDto.cs
@@ -0,0 +1,19 @@
+﻿namespace Praktikum542.DTOs
+{
+    public class ReviewDto
+    {
+        public int ReviewId { get; set; }
+
+        public int BookingId { get; set; }
+
+        public int TourId { get; set; }
+
+        public int Rating { get; set; }
+
+        public string? Comment { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+
+        public string? UserName { get; set; }
+    }
+}
